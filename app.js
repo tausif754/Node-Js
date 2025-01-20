4ff161d62588651429ded0945b112a2ba2eaa5cd
Tausif Ahmad <tausifahmad754@gmail.com>
2025-01-20 22:22:13 +0530

    http module to create a server

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+const http = require("http");
+
 // fs.readFile("data.txt", "utf8", (err, data) => {
 //   if (err) {
 //     console.log(err);
@@ -9,12 +11,22 @@ const fs = require("fs");
 //   }
 // });
 
-const content = "Hello World";
-fs.writeFile("output.txt", content, (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log("File written successfully\n");
-  }
+// const content = "Hello World";
+// fs.writeFile("output.txt", content, (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   } else {
+//     console.log("File written successfully\n");
+//   }
+// });
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World I am learning node js\n");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });

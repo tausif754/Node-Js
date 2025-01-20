5536498ccfd02d630d941cebe018c059fb7823c6
Tausif Ahmad <tausifahmad754@gmail.com>
2025-01-20 22:32:07 +0530

    Example of path module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 const http = require("http");
+const path = require("path");
 
 // fs.readFile("data.txt", "utf8", (err, data) => {
 //   if (err) {
@@ -21,12 +22,19 @@ const http = require("http");
 //   }
 // });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World I am learning node js\n");
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World I am learning node js\n");
+// });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+// server.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+
+// Example of path module
+const directory = "/user/local";
+const fileName = "example.txt";
+
+const fullPath = path.join(directory, fileName);
+console.log(fullPath);
